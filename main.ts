ffe8bf636759f9458ec1d5fa5dd0d82b18df048f
ChasKane <LawrenceCKane@gmail.com>
2024-10-02 00:23:49 -0700

    fix package issues, add toggle for showing icon

--- main.ts
+++ main.ts
@@ -1,20 +1,26 @@
-import { App, Editor, Notice, Plugin, PluginSettingTab, Setting, addIcon, MarkdownView } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, MarkdownView } from 'obsidian';
 
 interface AutoScrollSettings {
 	speed: number;
+	showRibbonIcon: boolean;
 }
 
 const DEFAULT_SETTINGS: AutoScrollSettings = {
-	speed: 0.2
+	speed: 0.2,
+	showRibbonIcon: true,
 }
 const ribbonActiveClassName = 'autoscroll-ribbon-active';
+const pluginId = 'autoscroll';
 export default class AutoScrollPlugin extends Plugin {
 	settings: AutoScrollSettings;
 	active: boolean = false;
 	intervalId: number;
 	pixelfractionCounter: number = 0;
+	ribbonIconEl: HTMLElement;
 
 	private stopScroll() {
+		this.ribbonIconEl.removeClass(ribbonActiveClassName);
+		new Notice('Stopping Auto Scroller');
 		window.clearInterval(this.intervalId);
 		this.active = false;
 	}
@@ -42,17 +48,16 @@ export default class AutoScrollPlugin extends Plugin {
 	}
 
 	async onload() {
-		this.active = false;
 		await this.loadSettings();
 
 		this.addCommand({
-			id: "autoscroller-toggle-scrolling",
+			id: "toggle-scrolling",
 			name: "Autoscroller: toggle scrolling",
 			callback: () => {
 				if (this.active) {
-					new Notice('Stopping Auto Scroller');
 					this.stopScroll()
 				} else {
+					this.ribbonIconEl.addClass(ribbonActiveClassName);
 					new Notice('Starting Auto Scroller');
 					this.active = true;
 					this.intervalId = this.registerInterval(window.setInterval(() => this.performScroll(), 10));
@@ -60,31 +65,44 @@ export default class AutoScrollPlugin extends Plugin {
 			},
 		})
 		this.addCommand({
-			id: "autoscroller-increase-speed",
+			id: "increase-speed",
 			name: "Autoscroller: increase speed",
-			callback: () => {
+			callback: async () => {
 				if (this.settings.speed >= 2) {
 					this.settings.speed = .1
 				} else {
 					// to mitigate precision issues (e.g. avoid .1 + .1 = .20000000001)
 					this.settings.speed = Math.round(this.settings.speed * 10 + 1) / 10
 				}
+				await this.saveSettings();
 				new Notice('Setting speed to ' + this.settings.speed);
 			},
 		})
 		this.addCommand({
-			id: "autoscroller-decrease-speed",
+			id: "decrease-speed",
 			name: "Autoscroller: decrease speed",
-			callback: () => {
+			callback: async () => {
 				if (this.settings.speed <= .1) {
-					this.settings.speed = 2
+					this.settings.speed = 2;
 				} else {
 					// to mitigate precision issues (e.g. avoid .1 + .1 = .20000000001)
-					this.settings.speed = Math.round(this.settings.speed * 10 - 1) / 10
+					this.settings.speed = Math.round(this.settings.speed * 10 - 1) / 10;
 				}
+				await this.saveSettings();
 				new Notice('Setting speed to ' + this.settings.speed);
 			},
 		})
+
+		console.log(this.settings.showRibbonIcon, this.ribbonIconEl)
+		if (this.settings.showRibbonIcon) {
+			this.ribbonIconEl = this.addRibbonIcon('double-down-arrow-glyph', `Auto Scroller (speed ${this.settings.speed})`, e => {
+				if (e.button === 0) { // left mouse button
+					app.commands.executeCommandById(`${pluginId}:toggle-scrolling`);
+				} else { // right mouse button
+					app.commands.executeCommandById(`${pluginId}:increase-speed`);
+				}
+			})
+		}
 
 		this.addSettingTab(new AutoScrollSettingTab(this.app, this));
 	}
@@ -118,9 +136,30 @@ class AutoScrollSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', { text: 'Settings for Autoscroll Plugin' });
 
 		new Setting(containerEl)
+			.setName('Show Ribbon Icon')
+			.addToggle(toggle => toggle
+				.setValue(true)
+				.onChange(value => {
+					this.plugin.settings.showRibbonIcon = value;
+					this.plugin.saveSettings();
+					if (value) {
+						this.plugin.ribbonIconEl = this.plugin.addRibbonIcon('double-down-arrow-glyph', `Auto Scroller (speed ${this.plugin.settings.speed})`, e => {
+							if (e.button === 0) { // left mouse button
+								app.commands.executeCommandById(`${pluginId}:toggle-scrolling`);
+							} else { // right mouse button
+								app.commands.executeCommandById(`${pluginId}:increase-speed`);
+							}
+						})
+					} else {
+						this.plugin.ribbonIconEl?.remove();
+					}
+				})
+			)
+
+		new Setting(containerEl)
 			.setName('Default scrolling speed')
 			.setDesc('The number of pixels to pass in 10 ms')
-			.addSlider((slider) => slider
+			.addSlider(slider => slider
 				.setLimits(.1, 2, .1)
 				.setValue(this.plugin.settings.speed)
 				.setDynamicTooltip()
